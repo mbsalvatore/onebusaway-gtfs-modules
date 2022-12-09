616f8d4ab40976ba66d6dfa6f22cf6e9fdf42523
Mitchell Salvatore <msalvatore@camsys.com>
2022-12-09 11:00:48 -0500

    test cleanup

--- onebusaway-gtfs/src/test/java/org/onebusaway/gtfs/model/MissingValueTest.java
+++ onebusaway-gtfs/src/test/java/org/onebusaway/gtfs/model/MissingValueTest.java
@@ -1,18 +1,3 @@
-/**
- * Copyright (C) 2012 Google, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.onebusaway.gtfs.model;
 
 import java.io.File;
@@ -45,9 +30,6 @@ import static org.junit.Assert.*;
 public class MissingValueTest {
 
     private MockGtfs _gtfs;
-
-    private static DecimalFormat _format = new DecimalFormat("00", new DecimalFormatSymbols(Locale.ENGLISH));
-
 
     private File _tmpDirectory;
 
--- onebusaway-gtfs/src/test/java/org/onebusaway/gtfs/model/StopTimeWithUnderscoreTest.java
+++ onebusaway-gtfs/src/test/java/org/onebusaway/gtfs/model/StopTimeWithUnderscoreTest.java
@@ -1,18 +1,3 @@
-/**
- * Copyright (C) 2012 Google, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.onebusaway.gtfs.model;
 
 import java.io.File;
@@ -43,9 +28,6 @@ public class StopTimeWithUnderscoreTest {
 
     private MockGtfs _gtfs;
 
-    private static DecimalFormat _format = new DecimalFormat("00", new DecimalFormatSymbols(Locale.ENGLISH));
-
-
     private File _tmpDirectory;
 
     @Before
@@ -53,7 +35,7 @@ public class StopTimeWithUnderscoreTest {
         _gtfs = MockGtfs.create();
 
         //make temp directory for gtfs writing output
-        _tmpDirectory = File.createTempFile("GtfsWriterMissingValueTest-", "-tmp");
+        _tmpDirectory = File.createTempFile("GtfsWriterStopTimeWithUnderScoreTest-", "-tmp");
         if (_tmpDirectory.exists())
             GtfsWriterTest.deleteFileRecursively(_tmpDirectory);
         _tmpDirectory.mkdirs();
